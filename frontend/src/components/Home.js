cda81994a7ce3ac3c12948306ce885842bb7f6d7
krishna-tikariya03 <krishnatikariya.md03@gmail.com>
2025-05-15 23:12:40 +0530

    Set up project dependancy and tailwind css. and ignite app.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,20 @@
 const Home = () =>{
     return (
         <>
-            <div>
-                <p>Hello</p>
-                <p>Test</p>
-            </div>
+            <nav>
+                <div>
+                    <p>Krishna Tikariya</p>
+                    <p>MERN Stack Developer</p>
+                </div>
+
+                <div>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Project</a></li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
